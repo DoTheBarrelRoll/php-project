6ad9d9df8dbcef8de316b370a4526acee52f1c34
DoTheBarrelRoll <32955643+DoTheBarrelRoll@users.noreply.github.com>
2018-11-02 11:15:49 +0200

    Päivitelty sivuja

--- koodi.php
+++ koodi.php
@@ -1,21 +1,43 @@
 <?php
 
-  $servername = "localhost";
-  $dbname = "mydb";
-  $username = "root";
-  $password = "";
+$connectstr_dbhost = '';
+$connectstr_dbname = 'mydb';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
+foreach ($_SERVER as $key => $value) {
+if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+continue;
+}
 
-  // Check connection
-  if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
-  }
+$connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+$connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+$connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+echo "dbhost: ".$connectstr_dbhost."<br>";
+echo "dbname: ".$connectstr_dbname."<br>";
+echo "dbusername: ".$connectstr_dbusername."<br>";
+echo "dbpassword: ".$connectstr_dbpassword."<br>";
+
+$conn = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$conn) {
+echo "Error: Unable to connect to MySQL." . PHP_EOL;
+echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+
+
+
   $email = $_POST["email"];
   $passu = $_POST["password"];
-  // Create database
-  $sql = "SELECT * FROM users WHERE email='$email' AND password='$passu'" ;
+
+  $sql = "SELECT * FROM userinfo WHERE email='$email' AND password='$passu'" ;
 
 
   $result = $conn->query($sql);
